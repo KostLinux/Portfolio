1a3b22e71e24f0027c0bb901dfea02ea4672d8c2
Martin Sidorov <martin.sidorov@wallester.com>
2023-12-12 20:02:14 +0200

    Make the hover effect on experience sections

--- js/experience.js
+++ js/experience.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
       element.className = `w-full md:w-1/2 my-4 px-4 ${index % 2 === 0 ? 'md:pr-2' : 'md:pl-2'}`;
       element.setAttribute('x-data', '{ open: false }');
       element.innerHTML = `
-        <div class="bg-white shadow rounded-lg p-6" @click="open = !open">
+        <div class="bg-white shadow rounded-lg p-6" @mouseover="open = true" @mouseout="open = false">
           <h3 class="text-xl font-bold mb-2">${item.title} at ${item.company}</h3>
           <p x-show="open" class="mb-2 transition duration-500">${item.duration}</p>
           <p x-show="open" class="transition duration-500">${item.description}</p>
