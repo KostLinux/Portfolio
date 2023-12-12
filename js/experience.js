73cb688a0c0dd6d0cc15098d573e8f100eea8557
KostLinux <githubchristofher@gmail.com>
2023-12-12 21:55:34 +0200

    Updates in experience

--- js/experience.js
+++ js/experience.js
@@ -2,23 +2,33 @@ window.addEventListener('load', function() {
   fetch('../experience.json')
     .then(response => response.json())
     .then(data => {
-    const container = document.getElementById('Experience');
-    const timeline = document.createElement('div');
-    timeline.className = 'flex flex-col md:flex-row md:flex-wrap';
+      const container = document.getElementById('Experience');
+      const timeline = document.createElement('ul');
+      timeline.className = 'timeline';
 
-    data.forEach((item, index) => {
-      const element = document.createElement('div');
-      element.className = `w-full md:w-1/2 my-4 px-4 ${index % 2 === 0 ? 'md:pr-2' : 'md:pl-2'}`;
-      element.setAttribute('x-data', '{ open: false }');
-      element.innerHTML = `
-        <div class="bg-white shadow rounded-lg p-6" @mouseover="open = true" @mouseout="open = false">
-          <h3 class="text-xl font-bold mb-2">${item.title} at ${item.company}</h3>
-          <p x-show="open" class="mb-2 transition duration-500">${item.duration}</p>
-          <p x-show="open" class="transition duration-500">${item.description}</p>
-        </div>
-      `;
-      timeline.appendChild(element);
+      data.forEach((item, index) => {
+        const element = document.createElement('li');
+        element.className = 'timeline-item';
+        element.style = `--accent-color:${item.color}`;
+      
+        const date = document.createElement('div');
+        date.className = 'date';
+        date.textContent = item.year;
+        element.appendChild(date);
+      
+        const title = document.createElement('div');
+        title.className = 'title';
+        title.textContent = `${item.title} at ${item.company}`;
+        element.appendChild(title);
+      
+        const descr = document.createElement('div');
+        descr.className = 'descr';
+        descr.textContent = item.duration;
+        element.appendChild(descr);
+      
+        timeline.appendChild(element);
+      });
+
+      container.appendChild(timeline);
     });
-    container.appendChild(timeline);
-  });
 });
